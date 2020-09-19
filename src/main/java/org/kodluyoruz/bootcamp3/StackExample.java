9243268a1bc8e96aa88daaa5888697ca9caa815a
Sila <sila.eryilmaz@mni.thm.de>
2020-09-19 22:50:07 +0300

    Case Study Pair Work Session 2

--- src/main/java/org/kodluyoruz/bootcamp3/StackExample.java
+++ src/main/java/org/kodluyoruz/bootcamp3/StackExample.java
@@ -2,6 +2,7 @@ package org.kodluyoruz.bootcamp3;
 
 import jdk.nashorn.internal.objects.annotations.Getter;
 import jdk.nashorn.internal.objects.annotations.Setter;
+import org.kodluyoruz.bootcamp3.exception.EmptyListException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +37,16 @@ public class StackExample <T>{
     }
 
 
+    public void popItem() {
+        if(arr.size() == 0){
+            throw new EmptyListException("List is empty!");
+        }
+        arr.remove(arr.size() - 1);
+    }
 
+    public <T> T peek() {
 
+        T value = (T) arr.get(arr.size()-1);
+        return value;
+    }
 }
--- src/test/java/org/kodluyoruz/bootcamp3/CaseStudyStack.java
+++ src/test/java/org/kodluyoruz/bootcamp3/CaseStudyStack.java
@@ -1,44 +1,111 @@
 package org.kodluyoruz.bootcamp3;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.kodluyoruz.bootcamp3.exception.EmptyListException;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.assertj.core.api.InstanceOfAssertFactories.INTEGER;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CaseStudyStack<T> {
 
-   private  StackExample sut = new StackExample();
+    StackExample sut = new StackExample();
 
-    @BeforeAll
-    public static <T> void setup(){
+
+
+    @BeforeEach
+    public <T> void setup(){
         List<T> list = new ArrayList<>();
+
+        T value1= (T) Integer.valueOf(1);
+        T value2= (T) Integer.valueOf(2);
+        T value3= (T) Integer.valueOf(3);
+        list.add(value1);
+        list.add(value2);
+        list.add(value3);
+        sut.setArr(list);
     }
 
     @Test
     public void pushItem_whenNewItemAdded_ShouldReturnCorrectSize(){
-        StackExample sut = new StackExample();
-        List<T> list = new ArrayList<>();
-        sut.setArr(list);
         //Act
         sut.pushItem(2);
         //Verify
-        assertThat(sut.getSize()).isEqualTo(1);
+        assertThat(sut.getSize()).isEqualTo(4);
     }
 
     @Test
     public void popItem_whenItemDeleted_ShouldReturnCorrectSize(){
-        //Arrange
-        StackExample sut = new StackExample();
-        List<T> list = new ArrayList<>();
-        sut.setArr(list);
         //Act
-
+        sut.pushItem(2);
+        sut.popItem();
         //Verify
+        assertThat(sut.getSize()).isEqualTo(3);
     }
 
+    @Test
+    public void pop_whenItemDeleted_ShouldDeleteFromTop(){
+        //Act
+        sut.pushItem(2);
+        sut.popItem();
+        //Verify
+        T expectedResult = (T) sut.getArr().get(sut.getSize()-1);
+        assertThat(expectedResult).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void peekOnStack_ShouldReturnItemFromTheTop(){
+        //Act
+        sut.pushItem(2);
+        sut.pushItem(3);
+        T expectedResult = (T) sut.peek();
+        //verify
+        assertThat(expectedResult).isEqualTo(3);
+    }
+
+    @Test
+    public void countList_ShouldReturnCountOfTheList(){
+        //Act
+        sut.pushItem(2);
+        sut.pushItem(3);
+        int count = sut.getSize();
+        //verify
+        assertThat(count).isEqualTo(5);
+    }
+
+    @Test
+    public void whenListEmpty_ShouldReturnEmptyListException(){
+        popItemForXTimes(3);
+        //Act
+        Throwable throwable = catchThrowable(() -> sut.popItem());
+        //verify
+        assertThat(sut.getArr()).isNotNull();
+        assertThat(throwable).isInstanceOf(EmptyListException.class).hasMessage("List is empty!");
+
+    }
+
+    @Test
+    public void peek_whenThereAreNoArgument_ShouldThrowNullPointerException(){
+        //Arrange
+        sut.setArr(null);
+        //Act
+        Throwable throwable = catchThrowable(() -> sut.peek());
+        //Verify
+        assertThat(throwable).isInstanceOf(NullPointerException.class);
+    }
+
+
+
+    public void popItemForXTimes(int count){
+        for(int i=0; i<count ; i++){
+            sut.popItem();
+        }
+    }
 }
